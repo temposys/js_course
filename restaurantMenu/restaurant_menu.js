9564efc3e9fce909e4551e51c6e3021b07f0f8dc
iana.sergieieva <iana.sergieieva@bahnhof.net>
2024-12-09 11:48:46 -0800

    restautant_menu: add prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $13', 'Pasta - $41', 'Burger - $12', 'Salmon - $12.50'];
+const dessertMenu = ['Cake - $3', 'Ice Cream - $3', 'Pudding - $4', 'Fruit Salad - $5'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
